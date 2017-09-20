bb756860dface0b06cb524f2b5a9388b43c6b8b7
John Crocker <j.crocker355@gmail.com>
2017-09-20 11:32:32 +0100

    Added config to tests

--- src/WinterIsComing.WebApi/App_Start/WebApiConfig.cs
+++ src/WinterIsComing.WebApi/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using WinterIsComing.WebApi.Services;
 
 namespace WinterIsComing.WebApi
 {
@@ -10,6 +12,8 @@ namespace WinterIsComing.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            RegisterContentNegotiator(config);
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +23,13 @@ namespace WinterIsComing.WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        public static void RegisterContentNegotiator(HttpConfiguration config)
+        {
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            jsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.None;            
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
         }
     }
 }
---
+++ src/WinterIsComing.WebApi/Services/JsonContentNegotiator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace WinterIsComing.WebApi.Services
+{
+    public class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter _jsonFormatter;
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
+        {
+            _jsonFormatter = formatter;
+        }
+
+        public ContentNegotiationResult Negotiate(Type type, HttpRequestMessage request, IEnumerable<MediaTypeFormatter> formatters)
+        {
+            return new ContentNegotiationResult(
+                _jsonFormatter,
+                JsonMediaTypeFormatter.DefaultMediaType
+                );
+        }
+    }
+}
